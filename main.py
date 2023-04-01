1f5b495b63b5e2bd1f50aa80c81a7e3c8a2102cf
Raivis Ilva <raivis.ilva@edu.rtu.lv>
2023-04-01 20:28:45 +0300

    velreiz

--- main.py
+++ main.py
@@ -11,40 +11,37 @@ class Query:
         else:
             self.number = int(query[1])
 
-
 def read_queries():
-    n = int(input().strip())
-    if not 1 <= n <= 10**5:
-        raise ValueError("Invalid value of N")
+    n = int(input())
     queries = []
     for i in range(n):
-        query = input().strip()
-        if not re.match(r"(add|del|find) \d{1,7} [a-zA-Z]{1,15}", query):
+        query = input().strip().split()
+        if len(query) == 2 and query[0] == "find":
+            queries.append(Query(query))
+        elif len(query) == 3 and query[0] == "add" and re.match("^[0-9]{1,7}$", query[1]) and re.match("^[a-zA-Z]{1,15}$", query[2]):
+            queries.append(Query(query))
+        elif len(query) == 2 and query[0] == "del" and re.match("^[0-9]{1,7}$", query[1]):
+            queries.append(Query(query))
+        else:
             raise ValueError("Invalid query format")
-        queries.append(Query(query.split()))
     return queries
+
+def process_queries(queries):
+    result = []
+    phone_book = {}
+    for query in queries:
+        if query.type == 'add':
+            phone_book[query.number] = query.name
+        elif query.type == 'del':
+            if query.number in phone_book:
+                del phone_book[query.number]
+        else:
+            name = phone_book.get(query.number, "not found")
+            result.append(name)
+    return result
 
 def write_responses(result):
     print('\n'.join(result))
-
-def process_queries(queries):
-    contacts = {}
-    result = []
-    # Keep a dictionary of all existing (i.e. not deleted yet) contacts.
-
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            contacts[cur_query.number] = cur_query.name
-        elif cur_query.type == 'del':
-            if cur_query.number in contacts:
-                del contacts[cur_query.number]
-        else:
-            if cur_query.number in contacts:
-                result.append(contacts[cur_query.number])
-            else:
-                result.append('not found')
-    return result
-
 
 if __name__ == '__main__':
     queries = read_queries()

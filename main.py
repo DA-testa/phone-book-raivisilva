e1f195a2376f2c9f5d287ae5dc5d40927bee57a0
Raivis Ilva <raivis.ilva@edu.rtu.lv>
2023-04-01 20:33:23 +0300

    varbut bus atrak?

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def read_queries():
         query = input().strip().split()
         if len(query) == 2 and query[0] == "find":
             queries.append(Query(query))
-        elif len(query) == 3 and query[0] == "add" and re.match("^[0-9]{1,7}$", query[1]) and re.match("^[a-zA-Z]{1,15}$", query[2]):
+        elif len(query) == 3 and query[0] == "add" and query[1].isdigit() and query[2].isalpha() and len(query[1]) <= 7 and len(query[2]) <= 15:
             queries.append(Query(query))
-        elif len(query) == 2 and query[0] == "del" and re.match("^[0-9]{1,7}$", query[1]):
+        elif len(query) == 2 and query[0] == "del" and query[1].isdigit() and len(query[1]) <= 7:
             queries.append(Query(query))
         else:
             raise ValueError("Invalid query format")
